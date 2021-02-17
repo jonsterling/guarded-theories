47efdd2e0020a265eddc045e22f363a500dcef97
Jonathan Sterling <jon@jonmsterling.com>
2021-02-16 21:31:36 -0500

    fun

---
+++ GuardedLF.v
@@ -0,0 +1,50 @@
+Require Import Unicode.Utf8 ssreflect.
+
+Set Primitive Projections.
+
+Record iso (A B : Type) :=
+  {intro : A → B;
+   elim : B → A;
+   eta : ∀ x, intro (elim x) = x;
+   beta : ∀ x, elim (intro x) = x}.
+
+Arguments intro [_] [_] _.
+Arguments elim [_] [_] _.
+
+Infix "≅" := iso (at level 100).
+Notation "◻" := Type.
+Notation "f ∘ g" := (λ x, f (g x)) (at level 10).
+
+Axiom later : Type → Type.
+Notation "▷ A" := (later A) (at level 60).
+
+Axiom next : ∀ {A}, A → ▷ A.
+Notation "next: x" := (next x) (at level 100).
+
+Module Later.
+  Axiom from_eq : ∀ {A} (a b : A), ▷ (a = b) → next a = next b.
+  Axiom to_eq : ∀ {A} (a b : A), next a = next b → ▷ (a = b).
+  Axiom ap : ∀ {A B}, later (A → B) → later A → later B.
+
+  Module ApNotation.
+    Infix "⊛" := ap (at level 50).
+  End ApNotation.
+
+  Import ApNotation.
+
+  Axiom ap_compute : ∀ {A B} (f : A → B) (x : A), next f ⊛ next x = next (f x).
+
+  Definition map {A B} : (A → B) → later A → later B :=
+    fun f x => next f ⊛ x.
+End Later.
+
+Export Later.ApNotation.
+Infix "<$>" := Later.map (at level 50).
+
+Fixpoint boom n :=
+  ▷ match n with
+    | 0 => False
+    | S n => boom n
+    end.
+
+Notation "n ⊩ P" := (boom n → P) (at level 100).
---
+++ Lang.v
@@ -0,0 +1,61 @@
+Require Import Unicode.Utf8 ssreflect.
+Require Import GuardedLF.
+
+Axiom tp : ◻.
+Axiom tm : tp → Type.
+
+Notation "⋆" := tp.
+Notation "[ A ]" := (tm A).
+
+Axiom bool : tp.
+Axiom arr : tp → tp → tp.
+Axiom rec : (tp → tp) → tp.
+
+Infix "⇒" := arr (right associativity, at level 60).
+Notation "rec: X ; B" := (rec (fun X => B)) (at level 100).
+
+Axiom tt : [bool].
+Axiom ff : [bool].
+Axiom def_rec : ∀ {F}, ▷ [F (rec F)] ≅ [rec F].
+Axiom def_arr : ∀ {A B}, ([A] → [B]) ≅ [A ⇒ B].
+Axiom θ : ∀ {A}, ▷ [A] → [A].
+
+Notation lam := (intro def_arr).
+Notation app := (elim def_arr).
+Notation fold := (intro def_rec).
+Notation unfold := (elim def_rec).
+
+Notation "fold: e" := (fold e) (at level 100).
+Notation "unfold: e" := (unfold e) (at level 100).
+Notation "θ: e" := (θ e) (at level 100).
+Notation "θ[ A ]" := (@θ A).
+Notation "lam: x ; e" := (lam (fun x => e)) (at level 100).
+Infix "@" := app (at level 60).
+
+Definition δ {A} : [A] → [A] :=
+  fun e => θ (next e).
+
+Definition θ_arr_rhs {A B} : ▷ [A ⇒ B] → [A ⇒ B] :=
+  fun e =>
+  lam: x;
+  θ: (fun f => f @ x) <$> e.
+
+Definition θ_rec_rhs {F} : ▷ [rec F] → [rec F] :=
+  fun e =>
+  fold: (θ ∘ unfold) <$> e.
+
+Axiom θ_arr : ∀ {A B}, θ[A ⇒ B] = θ_arr_rhs.
+Axiom θ_rec : ∀ {F}, θ[rec F] = θ_rec_rhs.
+
+Definition fixpoint {A} : [A ⇒ A] → [A] :=
+  let w := fun h : [(rec: X; X ⇒ A) ⇒ A] => h @ fold: next: lam: x; h @ x in
+  fun f => w (lam: x; f @ w (lam: y; (θ: unfold x) @ y)).
+
+Definition bot {A} : [A] := fixpoint (lam: x; x).
+
+Goal 0 ⊩ δ tt = bot.
+  move=> boom; compute.
+  rewrite ? beta θ_arr beta /Later.map Later.ap_compute.
+  f_equal; apply: Later.from_eq.
+  move: boom; by apply: Later.map.
+Qed.
